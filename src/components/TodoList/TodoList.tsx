077ff88cb85ab995d3d5816bd68772a35d594bc9
IgorGorskov <cuorier@gmail.com>
2024-08-02 20:34:28 +0300

    addd filter to TodoList

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { TodoItem } from "../TodoItem/TodoItem"
 import "./TodoList.css"
 
@@ -7,21 +7,49 @@ export interface TodoList{
     list: TodoItem[]
 }
 
-export const TodoList = ({todoList, onTodoSet, onTodoDone, onTodoDelete}) => {
+export const TodoList = ({todoList, onTodoSet}) => {
+    const [filter, setFilter] = useState("")
+
+    function toFilter(item: TodoItem) {
+        if (filter === "all"){
+            return true
+        }
+        if (filter === "notDone" && item.done === false){
+            return true
+        }
+        if (filter === "done" && item.done === true){
+            return true
+        }
+    }
+
+
     return(
+        
+    <>
+        <div className="filter">
+            <button onClick={()=>setFilter("all")} className="filter__button">
+                Все задачи
+            </button>
+            <button onClick={()=>setFilter("notDone")} className="filter__button">
+                Актуальные
+            </button>
+            <button onClick={()=>setFilter("done")} className="filter__button">
+                Сделанные
+            </button>
+        </div>
+
         <ul className="list">
-            {todoList.map((item) =>(
+            {todoList.filter(item => toFilter(item)).map((item) =>(
                 <li key={item.id}>
                     <TodoItem 
                         name = {item.name}
                         id = {item.id}
                         done={item.done}
                         onTodoSet={onTodoSet}
-                        onTodoDelete={onTodoDelete}
-                        onTodoDone={onTodoDone}
                     />
                 </li>)
             )}
         </ul>
+        </>
     )
 }
